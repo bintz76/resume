d1c640b154ef7d1a30360d35ce24b1afc220469e
yebin <djaak9604@gmail.com>
2023-11-25 04:03:57 +0900

    chore: 이력서 업데이트
    프로젝트 경험은 어떻게 추가하지이이이

--- apps/resume/src/pages/index.page.tsx
+++ apps/resume/src/pages/index.page.tsx
@@ -2,6 +2,7 @@ import { data as headerData, Header as HeaderType } from '../../_content/Header'
 import { data as otherExperienceDate, OtherExperience } from '../../_content/Other-Experience';
 import { data as skillsData, Skills } from '../../_content/Skills';
 import { data as workExperienceData, WorkExperience } from '../../_content/Work-Experience';
+// import { data as projectExperienceData, ProjectExperience } from '../../_content/Project-Experience';
 import Header from '../components/Header';
 import OtherExperienceSection from '../components/OtherExperienceSection';
 import SkillsSection from '../components/SkillsSection/SkillsSection';
